3360d98fd6d83d9075d047a0866de0c55b801d0d
Phil Crockett <contact@philcrockett.com>
2024-10-06 08:21:47 +0200

    newtarget script and rush template

---
+++ templates/rush.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env blarg
+
+depends_on rush/main
+
+PACKAGE_NAME="TODO"
+panic "not implemented yet"
+
+reached_if() {
+    # command -v some-bin
+}
+
+apply() {
+    rush get "${PACKAGE_NAME}"
+}
