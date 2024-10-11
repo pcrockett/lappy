9fbd414cd6294a269d78d463f76653c0c1a93e7b
Phil Crockett <contact@philcrockett.com>
2024-10-11 15:48:29 +0200

    better clock, battery info, more minimal qtile

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -157,13 +157,14 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                widget.TextBox("super+r to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
                 widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.Clock(format="%Y-%m-%d %a %H:%M"),
+                widget.TextBox("bat: "),
+                widget.Battery(),
+                widget.QuickExit(default_text="[X]"),
             ],
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
