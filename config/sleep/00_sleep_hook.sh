b0557068f7643c0bdd96e870059740188641c75a
Phil Crockett <contact@philcrockett.com>
2024-11-12 11:10:28 +0100

    fix: more reliably disable bluetooth on wake

--- config/sleep/00_sleep_hook.sh
+++ config/sleep/00_sleep_hook.sh
@@ -7,12 +7,27 @@ set -Eeuo pipefail
 PHASE="${1}"  # either "pre" or "post"
 # ACTION="${2}"  # "suspend", "hibernate", "hybrid-sleep", or "suspend-then-hibernate"
 
+# I'm not sure what environment this will run in. Don't rely on PATH to find your executables.
+RFKILL="/usr/bin/rfkill"
+AWK="/usr/bin/awk"
+GREP="/usr/bin/grep"
+
+active_bt_device_count() {
+    "${RFKILL}" --output TYPE,SOFT --noheadings \
+        | "${AWK}" '$1 == "bluetooth" { print $2 }' \
+        | "${GREP}" --fixed-strings --count unblocked
+}
+
 on_sleep() {
     echo "sleeping..."
 }
 
 on_wake() {
-    /usr/bin/rfkill block bluetooth
+    while [ "$(active_bt_device_count)" -ne 0 ];
+    do
+        "${RFKILL}" block bluetooth
+        sleep 1
+    done
 }
 
 main() {
