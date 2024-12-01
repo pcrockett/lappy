d3560c6009963ea73a7a58b47e4c772ba0a01951
Phil Crockett <contact@philcrockett.com>
2024-12-01 08:50:53 +0100

    fix(unclutter): restart on failure

--- targets/mouse/unclutter-started.bash
+++ targets/mouse/unclutter-started.bash
@@ -1,27 +1,20 @@
 #!/usr/bin/env blarg
 
 UNIT="unclutter.service"
-TIMEOUT_SECONDS=2
+REPO_PATH="${BLARG_CWD}/config/unclutter/${UNIT}"
+SYSTEM_PATH="${HOME}/.config/systemd/user/${UNIT}"
 
 depends_on unclutter-installed
 
 satisfied_if() {
-    test "$(systemctl --user is-enabled "${UNIT}")" == "enabled" \
+    test_symlink "${REPO_PATH}" "${SYSTEM_PATH}" \
+        && test "$(systemctl --user is-enabled "${UNIT}")" == "enabled" \
         && test "$(systemctl --user is-active "${UNIT}")" == "active"
 }
 
 apply() {
-    cat > "${HOME}/.config/systemd/user/${UNIT}" <<EOF
-[Unit]
-Description=Hide mouse after inactivity
-
-[Service]
-Type=exec
-ExecStart=/usr/bin/unclutter --timeout ${TIMEOUT_SECONDS}
-
-[Install]
-WantedBy=default.target
-EOF
+    rm -rf "${SYSTEM_PATH}"
+    ln --symbolic "${REPO_PATH}" "${SYSTEM_PATH}"
     systemctl --user daemon-reload
     systemctl --user enable --now "${UNIT}"
 }
