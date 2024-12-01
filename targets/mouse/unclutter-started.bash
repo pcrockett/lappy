2e1e6e954ec052b79427987d263a1270e52f9ac6
Phil Crockett <contact@philcrockett.com>
2024-12-01 08:54:44 +0100

    fix(unclutter): service change detection

--- targets/mouse/unclutter-started.bash
+++ targets/mouse/unclutter-started.bash
@@ -7,14 +7,13 @@ SYSTEM_PATH="${HOME}/.config/systemd/user/${UNIT}"
 depends_on unclutter-installed
 
 satisfied_if() {
-    test_symlink "${REPO_PATH}" "${SYSTEM_PATH}" \
+    files_are_same "${REPO_PATH}" "${SYSTEM_PATH}" \
         && test "$(systemctl --user is-enabled "${UNIT}")" == "enabled" \
         && test "$(systemctl --user is-active "${UNIT}")" == "active"
 }
 
 apply() {
-    rm -rf "${SYSTEM_PATH}"
-    ln --symbolic "${REPO_PATH}" "${SYSTEM_PATH}"
+    cp "${REPO_PATH}" "${SYSTEM_PATH}"
     systemctl --user daemon-reload
     systemctl --user enable --now "${UNIT}"
 }
