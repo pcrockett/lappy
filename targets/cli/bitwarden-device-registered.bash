0aae76abc120d46166e2c8dd641235ba3c2d2a6c
Phil Crockett <contact@philcrockett.com>
2024-12-02 20:28:24 +0100

    chore: remove old comment

--- targets/cli/bitwarden-device-registered.bash
+++ targets/cli/bitwarden-device-registered.bash
@@ -3,8 +3,6 @@
 depends_on pacman-deps-installed rbw-configured
 
 satisfied_if() {
-    # i'm not 100% sure this is the correct way to tell if it's satisfied -- need to test on a
-    # fresh install, but don't want to at the moment.
     test -f "${HOME}/.local/share/rbw/device_id"
 }
 
