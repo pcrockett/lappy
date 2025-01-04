c0fbb9812c3ad7d5ec5aa2e23b1e1ff98eb1394c
Phil Crockett <contact@philcrockett.com>
2025-01-04 16:40:29 -0600

    feat(gaming): plastic

---
+++ targets/gaming/plastic-installed.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env blarg
+# shellcheck disable=SC2034  # variables appear unused, but are used inside snippet
+
+PACKAGES=(
+    plastic
+    plastic_tui
+    alsa-lib
+    systemd-libs
+)
+
+snippet "packages-installed"
