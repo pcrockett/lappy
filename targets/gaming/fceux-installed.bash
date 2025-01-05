e5bbac5d58dcc056343280ad6f8865c7aa57e97a
Phil Crockett <contact@philcrockett.com>
2025-01-05 15:31:00 -0600

    feat: fceux instead of plastic for GUI

--- targets/gaming/fceux-installed.bash
+++ targets/gaming/fceux-installed.bash
@@ -2,9 +2,7 @@
 # shellcheck disable=SC2034  # variables appear unused, but are used inside snippet
 
 PACKAGES=(
-    plastic_tui
-    alsa-lib
-    systemd-libs
+    fceux
 )
 
 snippet "packages-installed"
