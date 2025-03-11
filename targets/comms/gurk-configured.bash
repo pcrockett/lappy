c6b4877ddb135d2748ea7d37dfd44234c1d68acb
Phil Crockett <contact@philcrockett.com>
2025-03-11 22:19:23 +0100

    chore(templating): migrate gurk to template utils

--- targets/comms/gurk-configured.bash
+++ targets/comms/gurk-configured.bash
@@ -5,15 +5,14 @@ SYSTEM_PATH=~/.config/gurk
 
 satisfied_if() {
     test_symlink "${REPO_PATH}" "${SYSTEM_PATH}" \
-        && test -f "${SYSTEM_PATH}/gurk.toml"
+        && test -f "${SYSTEM_PATH}/gurk.toml" \
+        && template_was_rendered "${REPO_PATH}/gurk.toml.template"
 }
 
 apply() {
     SIGNAL_PHONE_NUMBER="${SIGNAL_PHONE_NUMBER}" \
     HOME="${HOME}" \
-        envsubst \
-        < "${REPO_PATH}/gurk.toml.template" \
-        > "${REPO_PATH}/gurk.toml"
+        template_render "${REPO_PATH}/gurk.toml.template"
     rm -rf "${SYSTEM_PATH}"
     ln --symbolic "${REPO_PATH}" "${SYSTEM_PATH}"
 }
