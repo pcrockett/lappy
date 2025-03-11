fec30995d5602cc99f47cfd762cd7e03fde051b4
Phil Crockett <contact@philcrockett.com>
2025-03-11 22:54:42 +0100

    chore(templating): migrate aerc to templating utils

--- targets/email/aerc-configured.bash
+++ targets/email/aerc-configured.bash
@@ -6,8 +6,8 @@ REPO_CONFIG_DIR="${BLARG_CWD}/config/aerc"
 SYSTEM_CONFIG_DIR="${HOME}/.config/aerc"
 
 satisfied_if() {
-    test -f "${REPO_CONFIG_DIR}/accounts.conf"
-    test_symlink "${REPO_CONFIG_DIR}" "${SYSTEM_CONFIG_DIR}"
+    template_was_rendered "${REPO_CONFIG_DIR}/accounts.conf.template" \
+        && test_symlink "${REPO_CONFIG_DIR}" "${SYSTEM_CONFIG_DIR}"
 }
 
 apply() {
@@ -15,11 +15,11 @@ apply() {
     CONFIG_JSON="$(bw get item beekrpad.aerc.personal)"
 
     PERSONAL_EMAIL_ACCT_NAME="$(get_value account-name)" \
-        PERSONAL_EMAIL_IMAP="$(get_value imap)" \
-        PERSONAL_EMAIL_SMTP="$(get_value smtp)" \
-        PERSONAL_EMAIL_USER_FULL_NAME="$(get_value user-full-name)" \
-        PERSONAL_EMAIL_ADDRESS="$(get_value address)" \
-        envsubst < "${REPO_CONFIG_DIR}/accounts.conf.tmpl" \
+    PERSONAL_EMAIL_IMAP="$(get_value imap)" \
+    PERSONAL_EMAIL_SMTP="$(get_value smtp)" \
+    PERSONAL_EMAIL_USER_FULL_NAME="$(get_value user-full-name)" \
+    PERSONAL_EMAIL_ADDRESS="$(get_value address)" \
+        envsubst < "${REPO_CONFIG_DIR}/accounts.conf.template" \
         | with-umask u=rw,g=,o= dd "of=${REPO_CONFIG_DIR}/accounts.conf" status=none
 
     rm -rf "${SYSTEM_CONFIG_DIR}"
