f18564ce1b224ed77f65c0e24b030d32d18c4b3c
Phil Crockett <contact@philcrockett.com>
2025-08-29 21:35:28 +0200

    feat(dev): copier

--- targets/dev/main.bash
+++ targets/dev/main.bash
@@ -4,6 +4,7 @@ targets=(
     fly-cli-installed
     yaml-language-server-installed
     delta-installed
+    copier-installed
 )
 
 depends_on "${targets[@]}"
--- targets/rush/main.bash
+++ targets/rush/main.bash
@@ -4,6 +4,7 @@ targets=(
     rush-installed
     rush-configured
     rush-repo-cloned
+    dev/copier-installed  # needed for the `newpackage` script in rush repo
 )
 
 depends_on "${targets[@]}"
