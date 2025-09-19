0c7925d23453c2a359d831b330a7cd558a4081bd
Phil Crockett <contact@philcrockett.com>
2025-09-19 13:02:48 +0200

    feat(asdf): make shfmt globally available

--- targets/asdf/default-tools-installed.bash
+++ targets/asdf/default-tools-installed.bash
@@ -1,13 +1,14 @@
 #!/usr/bin/env blarg
 
-depends_on default-tools-configured shellcheck-plugin-installed
+depends_on default-tools-configured shellcheck-plugin-installed shfmt-plugin-installed
 
 satisfied_if() {
-  shellcheck --version
+  shellcheck --version && shfmt --version
 }
 
 apply() {
   cd ~ || panic "Unable to cd HOME?"
   asdf install || true
-  shellcheck --version &>/dev/null || panic "Shellcheck didn't install correctly."
+  shellcheck --version &>/dev/null || panic "shellcheck didn't install correctly."
+  shfmt --version &>/dev/null || panic "shfmt didn't install correctly."
 }
