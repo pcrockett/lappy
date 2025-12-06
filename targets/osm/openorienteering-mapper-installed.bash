63a148f6115847359b26a4393a27d19fa9f6a962
Phil Crockett <contact@philcrockett.com>
2025-12-06 18:24:34 +0100

    feat: openorienteering mapper

---
+++ targets/osm/openorienteering-mapper-installed.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env blarg
+# shellcheck disable=SC2034  # variables appear unused, but are used inside snippet
+
+depends_on openorienteering-repo-added
+
+PACKAGES=(
+  home_dg0yt_Arch_Community/openorienteering-mapper
+)
+
+snippet "packages-installed"
