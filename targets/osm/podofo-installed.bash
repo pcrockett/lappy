42ff88fdb5f0e7095cf139f6480f3499b85efd3f
Phil Crockett <contact@philcrockett.com>
2025-12-27 10:39:12 +0100

    feat(osm): podofo for generating georeferenced pdfs

--- targets/osm/podofo-installed.bash
+++ targets/osm/podofo-installed.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env blarg
 # shellcheck disable=SC2034  # variables appear unused, but are used inside snippet
 
-depends_on podofo-installed
+# For creating geospatial PDFs with QGIS
 
 PACKAGES=(
-  qgis
+  podofo
 )
 
 snippet "packages-installed"
