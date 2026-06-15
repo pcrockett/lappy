de09056877d529ad93b0ef9dfecbb4e4104ae33c
Phil Crockett <contact@philcrockett.com>
2026-06-15 12:19:28 +0200

    feat(perf): more power-saving TLP settings
    - sata link power: `med_power_with_dipm`
    - VM writeback upped from 5 seconds to 60
    - power saving for intel ME chipset

--- targets/perf/tlp-config-placed.bash
+++ targets/perf/tlp-config-placed.bash
@@ -2,15 +2,27 @@
 
 depends_on core/with-umask-installed
 
-FILE_NAME="charge-thresholds.conf"
+FILE_NAMES=(
+  charge-thresholds.conf
+  disk.conf
+  intel-me.conf
+)
 REPO_CONFIG_DIR="${BLARG_CWD}/config/tlp"
 SYSTEM_CONFIG_DIR="/etc/tlp.d"
 
 satisfied_if() {
-  files_are_same "${REPO_CONFIG_DIR}/${FILE_NAME}" "${SYSTEM_CONFIG_DIR}/${FILE_NAME}"
+  for file in "${FILE_NAMES[@]}"; do
+    files_are_same "${REPO_CONFIG_DIR}/${file}" "${SYSTEM_CONFIG_DIR}/${file}"
+  done
 }
 
 apply() {
   as_root with-umask u=rwx,g=rx,o=rx mkdir --parent "${SYSTEM_CONFIG_DIR}"
-  as_root with-umask u=rw,g=r,o=r cp "${REPO_CONFIG_DIR}/${FILE_NAME}" "${SYSTEM_CONFIG_DIR}"
+  for file in "${FILE_NAMES[@]}"; do
+    as_root install --mode u=rw,g=r,o=r "${REPO_CONFIG_DIR}/${file}" "${SYSTEM_CONFIG_DIR}/${file}"
+  done
+
+  if [ "$(systemctl is-active tlp.service)" == "active" ]; then
+    as_root systemctl restart tlp.service
+  fi
 }
