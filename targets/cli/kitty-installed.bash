480b79376ef174fd2688f9ae842e55034ac23e8c
Phil Crockett <contact@philcrockett.com>
2026-08-22 11:05:37 +0200

    chore(cli): record failed kitty experiment
    it'll be ready to go if this laptop dies though

---
+++ targets/cli/kitty-installed.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env blarg
+# shellcheck disable=SC2034  # variables appear unused, but are used inside snippet
+#
+# **THIS IS UNUSED.**
+#
+# kitty requires newer OpenGL drivers that don't support my ancient thinkpad X200.
+#
+# leaving this here for history, because apparently i've tried this experiment more
+# than once.
+
+PACKAGES=(
+  kitty
+  kitty-terminfo
+)
+
+snippet "packages-installed"
