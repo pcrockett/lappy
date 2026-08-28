cecc0fa737ee5e5700d480078c33a2f44798baf8
Phil Crockett <contact@philcrockett.com>
2026-08-28 06:29:08 +0200

    feat(tailscale): enable magic dns

--- targets/tailscale/tailscale-up.bash
+++ targets/tailscale/tailscale-up.bash
@@ -10,5 +10,5 @@ apply() {
   if ! tailscale up; then
     as_root tailscale up --operator "${USER}"
   fi
-  tailscale set --accept-dns=false --accept-routes --exit-node-allow-lan-access
+  tailscale set --accept-dns --accept-routes --exit-node-allow-lan-access
 }
